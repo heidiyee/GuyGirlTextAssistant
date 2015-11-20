08d03ccd125da95699210f2367db0cff151d2700
Roman Salazar Lopez <romansito18@gmail.com>
2015-11-19 16:51:59 -0800

    "still working on the patterns"

--- GuyGirlTextAssistant/Patterns.swift
+++ GuyGirlTextAssistant/Patterns.swift
@@ -10,11 +10,11 @@ import Foundation
 
 //MARK: Patterns (have to be all lower case)
 let prefixes = ["handsome", "chick", "couple"]
-let subjects = ["girl", "women", "hangout", "guy", "dude", "beautiful", "upset", "boyfriends"]
+let subjects = ["girl", "women", "hangout", "guy", "dude", "beautiful", "upset", "boyfriends", "guyfriends", "hoes", "bitches", "together", "roommates"]
 let verbs = ["dancing", "making out with", "go ", "out","run", "hike", "football", "soccer", "hate", "marriage", "love", "exclusive", "like\\syou", "like", "dinner", "party", "drink", "move\\sin\\swith me", "kisses", "luvya", "hugs"]
-let greetings = [ "how\\sare\\syou", "hi ", "hi", "hello", "bye", "later"]
+let greetings = [ "how", "hi ", "hi", "hello", "bye", "later"]
 let descriptions = ["jerk", "type", "pretty", "ugly", "mad", "handsome", "attractive", "good looking", "gorgeous", "manly", "gross", "princess", "kind", "awesome"]
-let places = ["club", "bar ", "barhop", "movies", "restaurant","coffeeshop", "coffee", "music", "tea", "chill", "comeover", "rade"]
+let places = ["club", "bar ", "barhop", "movies", "restaurant","coffeeshop", "coffee ", "music", "tea", "chill", "comeover", "rade", "beer", "happyhour"]
 
 
 
